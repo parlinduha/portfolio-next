912b53d6183c2064c9f7e6d77f83311da176c2ae
ParlinD <duhaperi@gmail.com>
2023-10-10 09:38:15 +0700

    add

---
+++ app/components/Container.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+
+ 
+const Container = ({children}) => {
+  return (
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8 lg:px-10">
+      {children}
+    </div>
+  )
+}
+
+export default Container
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
+import Container from './components/Container'
+import Navbar from './components/navbar/NavbarComp'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +15,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} id='body' >
+        <Container>
+          <Navbar />
+          <div className='pb-20 pt-28'>
+            </div>
+          {children}
+        </Container>
+      </body>
     </html>
   )
 }
